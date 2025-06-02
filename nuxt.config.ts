55537c81a18e55c03b8e1c1e20c365c84dbe639f
mateusbellei <dev.mbellei@gmail.com>
2025-06-02 01:05:12 -0300

    Add Tailwind CSS support and update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
-  modules: ["@nuxthq/ui"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/ui"],
+  css: ["~/assets/css/main.css"],
   devtools: { enabled: true },
+  tailwindcss: {
+    exposeConfig: true,
+  },
 });
