7571432f237d6321c9865241614566d1ca21b021
mateusbellei <dev.mbellei@gmail.com>
2025-06-02 00:35:44 -0300

    first commit

---
+++ electron/main.js
@@ -0,0 +1,23 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+    },
+  });
+
+  win.loadURL("http://localhost:3000");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  modules: ["@nuxthq/ui"],
+  devtools: { enabled: true },
+});
